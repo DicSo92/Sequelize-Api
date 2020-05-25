bb849acf07f8f740743b1017f4c2e53a6a2e8978
Luzzi Charly <luzzi.charly@gmail.com>
2020-05-25 12:05:56 +0200

    Sign up form && axios install

--- vue-front/src/router/index.js
+++ vue-front/src/router/index.js
@@ -2,28 +2,34 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
+
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/signin',
+        name: 'signin',
+        component: () => import('../views/Signin.vue')
+    },
+    {
+        path: '/signup',
+        name: 'signup',
+        component: () => import( '../views/Signup.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ vue-front/src/views/Signin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="Signin">
+        <h1>Sign In</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Signin"
+    }
+</script>
+
+<style scoped>
+
+</style>
