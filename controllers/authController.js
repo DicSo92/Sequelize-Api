be1eb0df87bb8c08eb265026df4aea0652458d1f
Luzzi Charly <luzzi.charly@gmail.com>
2020-06-09 12:50:43 +0200

    SignUp jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,50 @@ const { User } = require('../models')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 
+const jwt = require('jsonwebtoken')
+
 const bcrypt = require('bcrypt')
 
 const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/
 
+const jwtSecret = 'awesome-secret-string'
+const generateJwt =(id, username, email, isAdmin) =>{
+    return jwt.sign({
+        iss: 'http://localhost:8080',
+        id,
+        username,
+        email,
+        isAdmin,
+        exp: parseInt(Date.now() / 1000 + 60 * 60)// Ce JWT expirera dans 1h
+    }, jwtSecret)
+}
+
+const checkJwt = (req, res, next) => {
+    try {
+        if (!req.header('Authorization'))
+        {
+            throw "No Authorization Header in the request !"
+        }
+        const authorizationParts = req.header('Authorization').split(' ')
+        let token = authorizationParts[1]
+        jwt.verify(token, jwtSecret, (error, decodeToken) => {
+            if (error) {
+                throw error
+            }
+            req.token = decodeToken
+            next()
+        })
+    }
+    catch (e) {
+        console.log(e)
+        res.status(401).json({
+            msg : 'Acces Denied ! (JWT)',
+            event: e
+        })
+    }
+}
 
 module.exports = {
     signup(req, res) {
@@ -43,21 +81,27 @@ module.exports = {
                 })
             }
 
+            const hashPassword = bcrypt.hashSync(password, 10)
+
             User.findOrCreate({
                 where: {
                     [Op.or]: [{username}, {email}]
                 },
                 defaults: {
-                    username, password, email, isAdmin: false
+                    username, password: hashPassword, email, isAdmin: false
                 },
                 attributes: [
                     'id', 'email', 'username'
                 ]
             }).then(resultUser => {
                 if (resultUser[1]) {
+                    const { username, id, email, isAdmin } = resultUser[0]
+                    const token = generateJwt(id, username, email, isAdmin)
+
                     return res.status(201).json({
-                        msg: 'Nouvel utilisateur créé !',
-                        user: resultUser[0]
+                        msg: 'New User Created !',
+                        user: resultUser[0],
+                        token
                     })
                 } else {
                     return res.status(422).json({
@@ -72,7 +116,7 @@ module.exports = {
     signin(req, res) {
         const { body: { username } } = req
         return res.status(200).json({
-            msg: `Bienvenue ${ username } !`
+            msg: `Welcome ${ username } !`
         })
     }
 }
